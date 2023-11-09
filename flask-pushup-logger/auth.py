fecbf91a3f0bc6c58ede6ec12c7d788b6acc9e2b
Touch Nguonchhay <nguonchhay.touch@gmail.com>
2023-11-09 18:44:27 +0700

    [TASK] Flask login and register form

--- flask-pushup-logger/auth.py
+++ flask-pushup-logger/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, request, redirect
 
 auth = Blueprint('auth', __name__)
 
@@ -8,9 +8,30 @@ def register_page():
     return render_template('register.html')
 
 
+@auth.route('/signup', methods=['POST'])
+def signup():
+    fullname = request.form.get('fullname')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    print(fullname, email, password)
+
+    return redirect(url_for('auth.login_page'))
+
+
 @auth.route('/login')
 def login_page():
     return render_template('login.html')
+
+
+@auth.route('/login', methods=['POST'])
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    print(email, password)
+
+    return redirect(url_for('main.profile_page'))
 
 
 @auth.route('/logout')
